e8444142a6c60c8892a0f8a715098101e58af955
Jake Jarrett <jake.jarrett@pointsbet.com>
2019-01-16 18:45:04 +1100

    Revert changes

--- src/android/AppboyReceiver.java
+++ src/android/AppboyReceiver.java
@@ -15,9 +15,8 @@ public class AppboyReceiver extends BroadcastReceiver {
   public void onReceive(Context context, Intent intent) {
     String action = intent.getAction();
 
-    Log.v(TAG, action);
 
-    if ( action.equals("com.appboy.ui.intent.APPBOY_NOTIFICATION_OPENED") ) {
+    if ( action.equals("qq") ) {
         Log.v(TAG, "intent : "+intent);
 
         PackageManager pm = context.getPackageManager();
